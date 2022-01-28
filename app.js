8cc43e9837da6cddbc7512a5c11d94df1e63ff96
David Melsheimer <dave.melshman@gmail.com>
2022-01-27 21:23:51 -0500

    styling the pages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 const { get, redirect } = require('express/lib/response');
 const { update } = require('./models/campground');
 
@@ -16,6 +17,8 @@ db.once('open', () => {
 });
 
 const app = express();
+
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -27,7 +30,7 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', {campgrounds});
 }); 
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.render('home');
 });
            
@@ -42,6 +45,8 @@ app.get('/campgrounds/:id', async (req, res) => {
 });
 
 app.post('/campgrounds', async (req, res) => {
+    req.body.campground.image = `https://picsum.photos/501/374`;
+    req.body.campground.description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis incidunt suscipit officiis consequatur blanditiis! Ut eveniet ducimus libero distinctio veniam provident ullam praesentium ratione non delectus dolores consectetur, ipsam quaerat! Eum ullam quibusdam suscipit.'
     const newCamp = new Campground(req.body.campground);
     await newCamp.save();
     console.log(newCamp);
@@ -55,10 +60,19 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
 
 app.put('/campgrounds/:id', async (req, res) => {
     const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground}, {new: true});
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    //was not working because I was passing an object to it!
+    res.redirect('/campgrounds');
 });
 
 app.listen(3000, () => {
     console.log('serving on 3000');
 });
+
+// ------------------>>>>>wORK ON MIDDLEWARE DEMO 
--- seeds/index.js
+++ seeds/index.js
@@ -18,12 +18,16 @@ const sample = (helperList) => {
 const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i ++){
+        const price = rand(30) + 10;
         const camp = new Campground({
             location: `${cities[rand(1000)].city}, ${cities[rand(1000)].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            // image: `https://picsum.photos/700/500?grayscale&blur=2`
+            image: `https://picsum.photos/5${price}/7${50+price}`,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis incidunt suscipit officiis consequatur blanditiis! Ut eveniet ducimus libero distinctio veniam provident ullam praesentium ratione non delectus dolores consectetur, ipsam quaerat! Eum ullam quibusdam suscipit.',
+            price: price
         });
         await camp.save();
     }
 }
 seedDB().then(() => db.close());
-
